0918fcc262bc42dace571e54c18c889cd664aec6
michael.daniel <michael@pcsg.de>
2019-02-03 23:23:23 +0100

    feat: Typography chagned. feat: Who ist namerobot for section designed. feat: HeroHeader works better. feat: Mega Menu improvement.

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -254,6 +254,19 @@ module.exports = {
   */
 
   textSizes: {
+    'xs': '.875rem',    // 14px
+    'sm': '1rem',     // 16px
+    'base': '1.125rem',   // 18px
+    'lg': '1.25rem',    // 20px
+    'xl': '1.5rem',    // 24px
+    '2xl': '1.875rem',  // 30px
+    '3xl': '2.25rem',   // 36px
+    '4xl': '2.625rem',  // 36px
+    '5xl': '3.5rem',    // 48px
+    '6xl': '4rem',    // 48px
+  },
+
+  /*textSizes: {
     'xs': '.75rem',     // 12px
     'sm': '.875rem',    // 14px
     'base': '1rem',     // 16px
@@ -264,7 +277,7 @@ module.exports = {
     '4xl': '2.25rem',   // 36px
     '5xl': '2.625rem',  // 36px
     '6xl': '3.5rem',    // 48px
-  },
+  },*/
 
 
   /*
