87ce906c49d3f07d1d4351389b6e8c96899fe534
michael.daniel <michael@pcsg.de>
2019-02-10 22:17:05 +0100

    feat: All layouts were adjusted to tailwind css. feat: Scroll to top icon.

--- bin/init.js
+++ bin/init.js
@@ -6,12 +6,58 @@ window.addEvent("domready", function () {
         new Fx.Scroll(window).toTop();
     });
 
+
+
     require(['qui/QUI', 'utils/Controls'], function (QUI, Controls) {
         QUI.addEvent("onError", function (msg, url, linenumber) {
             console.error(msg);
             console.error(url);
             console.error('LineNo: ' + linenumber);
         });
+
+
+        /**
+         * toTop button
+         */
+        if (document.getElements('[href=#top]')) {
+            var toTop         = document.getElements('[href=#top]'),
+                buttonVisible = false;
+
+            // show on load after 1s delay
+            if (QUI.getScroll().y > 150) {
+                toTop.addClass('toTop__show');
+                buttonVisible = true;
+            }
+
+            // show button toTop after scrolling down
+            QUI.addEvent('scroll', function () {
+                if (QUI.getScroll().y > 150) {
+                    if (!buttonVisible) {
+                        toTop.addClass('toTop__show');
+                        buttonVisible = true;
+                    }
+                    return;
+                }
+
+                if (!buttonVisible) {
+                    return;
+                }
+                toTop.removeClass('toTop__show');
+                buttonVisible = false;
+            });
+
+            // scroll to top
+            toTop.addEvent('click', function (event) {
+                event.stop();
+                new Fx.Scroll(window).toTop();
+            });
+        }
+
+
+        var HeaderBar = document.getElement('.header-bar');
+
+        if (HeaderBar) {
+        }
 
 
     });
